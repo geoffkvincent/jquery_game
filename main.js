428009d7a3fb7241e69784070a36b63233ae45fc
Geoff Vincent <geoffkvincent@gmail.com>
2018-08-23 17:29:45 -0600

    added images and started html

---
+++ main.js
@@ -0,0 +1,7 @@
+//matching game
+//choose three images
+//make number var for each image
+//split screen 9 images both side
+//two arrays - computer array user array
+//page refresh randomize both arrays
+//
