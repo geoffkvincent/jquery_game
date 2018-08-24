65e9ee2a59159c0caf1a967ed92db5af23ff18fe
Geoff Vincent <geoffkvincent@gmail.com>
2018-08-24 07:05:16 -0600

    js not working

--- main.js
+++ main.js
@@ -3,17 +3,41 @@
 //page refresh randomize both arrays
 //
 
-var options_a = ['cage_a', 'fieri_a', 'hoff_a']
-var options_b = ['cage_b', 'fieri_b', 'hoff_b']
 
+var image_aray = ['cage_a', 'fieri_a', 'hoff_a', 'cage_b', 'fieri_b', 'hoff_b']
 var cage_a = document.getElementById('cage_a')
 var fieri_a = document.getElementById('fieri_a')
 var hoff_a = document.getElementById('hoff_a')
 var cage_b = document.getElementById('cage_b')
 var fieri_b = document.getElementById('fieri_b')
 var hoff_b = document.getElementById('hoff_b')
+var side_a = document.getElementByClassName('side_a')
+var individual_a = document.getElementByClassName('individual_a')
+document.write(random)
+random = image_array[Math.floor(Math.random() * image_array.length)]
+// random = options_b[Math.floor(Math.random() * options_b.length)]
+individual_a.innerHTML = random 
+function side_a () {
+random = image_array[Math.floor(Math.random() * image_array.length)]
 
+}
 
+side_a.addEventListener('click', function() {
+  
+  
+  
 
+// cage_a.addEventListener("click", myScript);
+// function side_a_fade (){
+//   cage_a.fadeIn(10000)
+// }
+
+// function hideCards() {
+//   setTimeout( function() {
+//     $('.card').children('i').toggleClass('hidden');
+//   }, 3000);
+// }
+
+// var_name.innerHTML = ""
 
 // document.getElementsByClassName()
